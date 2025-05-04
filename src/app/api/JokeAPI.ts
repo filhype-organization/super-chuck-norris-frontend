56599ea70e5ebecf3dbb911c867cedf9cae3410b
Philippe BERNARD <filhype.bernard@gmail.com>
2025-05-04 11:09:02 +0200

    fix var env

--- src/app/api/JokeAPI.ts
+++ src/app/api/JokeAPI.ts
@@ -3,18 +3,29 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Joke} from '../models/Joke';
 
+
+
+interface AppConfig {
+  API_URL?: string; 
+}
+
+declare global {
+  interface Window {
+    APP_CONFIG: AppConfig;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class JokeAPI {
   #http = inject(HttpClient);
   #headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   }
-  #uri = '/getRandomJoke';
-  #url = import.meta.env['NG_APP_API_URL'];
+  #uri = '/api/jokes/v1/getRandomJoke';
+  #url = window.APP_CONFIG?.API_URL || '';
 
   getRandomJoke(){
     return this.#http.get(this.#url + this.#uri, {headers: this.#headers}) as Observable<Joke>;
