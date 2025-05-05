79ca3da2a6b4b927ac7cf2b9769e31658dd65d49
LeesonLee <filhype.bernard@gmail.com>
2025-05-05 21:02:25 +0200

    Replace static endpoint backend by env var
    * build for arm and amd
    
    * change image caddy
    
    * add ngx-env/builder
    
    * add env var
    
    * del env file
    
    * fix var env

--- src/app/api/JokeAPI.ts
+++ src/app/api/JokeAPI.ts
@@ -1,22 +1,33 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
 import {Joke} from '../models/Joke';
+
+
+
+interface AppConfig {
+  API_URL?: string; 
+}
+
+declare global {
+  interface Window {
+    APP_CONFIG: AppConfig;
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class JokeAPI {
   #http = inject(HttpClient);
   #headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   }
-  #uri = '/getRandomJoke';
+  #uri = '/api/jokes/v1/getRandomJoke';
+  #url = window.APP_CONFIG?.API_URL || '';
 
   getRandomJoke(){
-    return this.#http.get(environment.apiURL + this.#uri, {headers: this.#headers}) as Observable<Joke>;
+    return this.#http.get(this.#url + this.#uri, {headers: this.#headers}) as Observable<Joke>;
   }
 }
