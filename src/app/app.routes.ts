0717ed31e2f80d0d5cf7433a67040a19f8e6bc84
Philippe BERNARD <filhype.bernard@gmail.com>
2025-06-29 09:36:06 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import {ChuckSearchComponent} from './components/chuck-search/chuck-search.component';
 import {UnauthorizedComponent} from './components/unauthorized/unauthorized.component';
-import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
-  {path: '', component: ChuckSearchComponent, canActivate: [authGuard]},
+  {path: '', component: ChuckSearchComponent},
   {path: 'unauthorized', component: UnauthorizedComponent},
   {path: 'forbidden', component: UnauthorizedComponent},
   {path: '**', redirectTo: ''}
