93801032d6b8d6b2c6901da7a0cafceded9fd6c0
Philippe BERNARD <filhype.bernard@gmail.com>
2025-06-29 10:24:17 +0200

    add variable clientid

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,12 +2,13 @@ import { OpenIdConfiguration, LogLevel } from 'angular-auth-oidc-client';
 
 const apiUrl = import.meta.env['NG_APP_API_URL'] || 'http://localhost:8080';
 const authUrl = import.meta.env['NG_APP_AUTH_URL'] || 'http://localhost:8180';
+const clientId = import.meta.env['NG_APP_CLIENT_ID'] || 'front';
 
 export const authConfig: OpenIdConfiguration = {
   authority: `${authUrl}/realms/app`,
   redirectUrl: window.location.origin,
   postLogoutRedirectUri: window.location.origin,
-  clientId: 'front',
+  clientId: clientId,
   scope: 'openid',
   disablePkce: false,
   responseType: 'code',
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,11 +1,10 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const oidcSecurityService = inject(OidcSecurityService);
-  const router = inject(Router);
   
   return oidcSecurityService.checkAuth().pipe(
     tap((loginResponse) => {
