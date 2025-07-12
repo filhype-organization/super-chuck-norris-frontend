b8697635c61267240214c42246d987398e4e2c2c
Philippe BERNARD <philippe1.bernard@orange.com>
2025-07-12 10:20:42 +0200

    changement de logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { JokeAdminComponent } from './components/joke-admin/joke-admin.component
 export const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [authGuard]},
   {path: 'admin-joke', component: JokeAdminComponent, canActivate: [authGuard, adminGuard]},
-  {path: 'admin-protected', component: AdminPanelComponent, canActivate: [authGuard, adminGuard]},
+  {path: 'admin-protected', component: AdminPanelComponent, canActivate: [authGuard]},
   {path: 'unauthorized', component: UnauthorizedComponent},
   {path: 'forbidden', component: UnauthorizedComponent},
   {path: '**', redirectTo: ''}
